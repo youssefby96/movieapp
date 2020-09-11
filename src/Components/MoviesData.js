26980d4eb84a91cc3323c1137e0807dd267e8c9e
unknown <youssefby3@gmail.com>
2020-09-11 01:46:59 -0700

    movieapp

---
+++ src/Components/MoviesData.js
@@ -0,0 +1,69 @@
+export const moviesData = [
+  {
+    id: Math.random(),
+    image:
+      'https://vignette.wikia.nocookie.net/xmenmovies/images/9/94/Deadpool_Textless.jpg/revision/latest?cb=20200608174908',
+    rating: 4,
+    name: "Deadpool",
+    date: '2014',
+    type: 'Action | Crime',
+    description:
+      "A relentlessly-paced hybrid of gritty crime thriller and Lovecraftian supernatural horror, The Devil's Mile follows a trio of kidnappers who take an ill-advised detour en route to deliver...",
+  },
+  {
+    id: Math.random(),
+    image:
+      'https://upload.wikimedia.org/wikipedia/en/3/34/TheNunPoster.jpg',
+    rating: 5,
+    name: 'The NUN',
+    date: '2018',
+    type: 'Western',
+    description:
+      'A mysterious stranger with a harmonica joins forces with a notorious desperado to protect a beautiful widow from a ruthless assassin working for the railroad.',
+  },
+  {
+    id: Math.random(),
+    image:
+      'https://cdn.onebauer.media/one/empire-images/features/59ad387b2ca1eaf217a24e4d/It.jpg?quality=50&width=1800&ratio=16-9&resizeStyle=aspectfill&format=jpg',
+    rating: 4,
+    name: 'Clown',
+    date: '2019',
+    type: 'Crime | Drama',
+    description:
+      'A mentally unstable veteran works as a nighttime taxi driver in New York City, where the perceived decadence and sleaze fuels his urge for violent action by attempting to liberate a presidential campaign worker and an underage prostitute.',
+  },
+  {
+    id: Math.random(),
+    image:
+      'https://images-na.ssl-images-amazon.com/images/I/71k38%2ByUiXL._SL1417_.jpg',
+    rating: 4,
+    name: 'Fast And Furious',
+    date: '1979',
+    type: 'Action | Adventure',
+    description:
+      'In a self-destructing world, a vengeful Australian policeman sets out to stop a violent motorcycle gang.',
+  },
+  {
+    id: Math.random(),
+    image:
+      'https://images-na.ssl-images-amazon.com/images/I/91qTMobYF2L._AC_SL1500_.jpg',
+    rating: 3,
+    name: 'Little Miss Sunshine',
+    date: '2006',
+    type: 'The Purge',
+    description:
+      'A family determined to get their young daughter into the finals of a beauty pageant take a cross-country trip in their VW bus.',
+  },
+  
+  // {
+  //   id: Math.random(),
+  //   image:
+  //     "https://i.pinimg.com/originals/3e/f1/ba/3ef1baaaceb5a95c4f57a7cb2393b39d.jpg",
+  //   rating: 5,
+  //   name: "The Wolf Of Wall Street",
+  //   date: "2013",
+  //   type: " Biography | Crime | Drama  ",
+  //   description:
+  //     "Based on the true story of Jordan Belfort, from his rise to a wealthy stock-broker living the high life to his fall involving crime, corruption and the federal government."
+  // }
+];
